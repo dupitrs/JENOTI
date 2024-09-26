79814ab3ecf4418b2770d282ff19222f13525383
Raivis Reinfelds <117306753+dupitrs@users.noreply.github.com>
2024-09-26 13:56:18 +0000

    Raivis Reinfelds

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById('darkModeToggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
