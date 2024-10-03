3519c8851079a04de48efc6af37533fea34758d3
Raivis Reinfelds <117306753+dupitrs@users.noreply.github.com>
2024-10-03 13:51:40 +0000

    Raivis Reinfelds

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const body = document.body;
 
 toggleButton.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
+
 });
 
 
@@ -12,27 +13,49 @@ toggleButton.addEventListener('click', () => {
 
 
 // Get the modal element
-var modal = document.getElementById("myModal");
+var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
 
 // Get the button that opens the modal
-var btn = document.getElementById("learnMoreBtn");
+var btn1 = document.getElementById("learnMoreBtn1");
+var btn2 = document.getElementById("learnMoreBtn2");
+var btn3 = document.getElementById("learnMoreBtn3");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+btn1.onclick = function() {
+    modal1.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    modal1.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
+
+
+
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
     }
 }
