777f3c68a70cac406885e85864543339841f2c0c
Raivis Reinfelds <117306753+dupitrs@users.noreply.github.com>
2024-10-07 20:21:05 +0000

    Raivis Reinfelds

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const toggleButton = document.getElementById('darkModeToggle');
+const toggleSwitch = document.getElementById('darkModeToggle');
 const body = document.body;
 
-toggleButton.addEventListener('click', () => {
+toggleSwitch.addEventListener('change', () => {
   body.classList.toggle('dark-mode');
-
 });
 
 
@@ -12,50 +11,45 @@ toggleButton.addEventListener('click', () => {
 
 
 
-// Get the modal element
-var modal1 = document.getElementById("myModal1");
-var modal2 = document.getElementById("myModal2");
-var modal3 = document.getElementById("myModal3");
-
-// Get the button that opens the modal
-var btn1 = document.getElementById("learnMoreBtn1");
-var btn2 = document.getElementById("learnMoreBtn2");
-var btn3 = document.getElementById("learnMoreBtn3");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn1.onclick = function() {
-    modal1.style.display = "block";
+// Function to open a modal by ID
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal1.style.display = "none";
+// Function to close a modal by ID
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// Assign each button to open the corresponding modal
+document.getElementById("learnMoreBtn1").onclick = function() {
+    openModal("myModal1");
+};
+document.getElementById("learnMoreBtn2").onclick = function() {
+    openModal("myModal2");
+};
+document.getElementById("learnMoreBtn3").onclick = function() {
+    openModal("myModal3");
+};
+
+// Assign each close button to close the corresponding modal
+var closeButtons = document.getElementsByClassName("close");
+Array.from(closeButtons).forEach((button, index) => {
+    button.onclick = function() {
+        closeModal(`myModal${index + 1}`);
+    };
+});
+
+// Close modal if user clicks outside of it
 window.onclick = function(event) {
-    if (event.target == modal1) {
-        modal1.style.display = "none";
-    }
-}
+    var modals = ["myModal1", "myModal2", "myModal3"];
+    modals.forEach(modalId => {
+        var modal = document.getElementById(modalId);
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+};
 
-
-
-btn1.onclick = function() {
-    modal1.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal1.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal1) {
-        modal1.style.display = "none";
-    }
-}
